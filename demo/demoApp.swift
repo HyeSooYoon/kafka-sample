a733468ba36493995f17096b86b7c95db0c9c601
HyeSooYoon <yhs1790@naver.com>
2024-01-29 17:26:07 +0900

    첫번째 코드

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by HYESOO on 2023/04/19.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
